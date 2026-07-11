84c8902864fb169d7513162ea52b1c9265dc7722
Pukhraj24 <goenkapukhraj68@gmail.com>
2026-07-11 18:15:30 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version of javascript
+
+// alert(3+3) //we are useing nodejs,not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Pukhraj")
+
+//datatypes
+
+let name="Pg" //string
+let age=18 //number
+let isLoggedIn = false //boolean
+let state= null //null
+
+/*
+number => upto 2 to power 53
+bigint 
+string =>""
+boolen
+null => standalone value to tell something is empty
+undefined => value not assigned
+symbol=> uniqueness
+*/
+
+//object
+console.log(typeof null);
+console.log(typeof undefined);
